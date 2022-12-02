b5bf6a8c4fd35dcb772158d29842c461012652a8
leeseungwoo <vugil@naver.com>
2022-12-02 14:20:05 +0900

    main-replication 적용코드

---
+++ src/main/java/com/example/replicationtest/ReplicationtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.replicationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicationtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplicationtestApplication.class, args);
+    }
+
+}
